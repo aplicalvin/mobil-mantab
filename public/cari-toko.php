a524eaa179b6900183e535f09131449e19a52a2e
calvinsims <thecalvins.simbolon@gmail.com>
2022-11-28 01:31:09 +0700

    Menambah halaman cari-mobil

--- public/cari-toko.php
+++ public/cari-toko.php
@@ -3,20 +3,6 @@
   <head>
     <?php include("components/metadata.php"); ?>
 
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>
-      MobilMantab - Mobil Listrik Berkualitas, Lebih Hemat, Ramah Lingkungan
-    </title>
-    <link rel="shortcut icon" href="assets/Favicon.png" type="image/x-icon" />
-    <!-- css -->
-    <link rel="stylesheet" href="css/output.css" />
-    <link rel="stylesheet" href="css/suplement.css" />
-
-    <!-- SwiperJS -->
-    <link rel="stylesheet" href="swiperjs/swiper-bundle.min.css" />
-
     <!-- Leaflet -->
     <link
       rel="stylesheet"
